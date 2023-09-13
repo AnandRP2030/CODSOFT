c36849aff823afa0c56dd1ef99d28b514ada32f5
AnandRP2030 <anand.rp2030@gmail.com>
2023-09-13 21:07:44 +0530

    database connected with server

--- Travel-Booking-System/Server/index.js
+++ Travel-Booking-System/Server/index.js
@@ -1,10 +1,23 @@
-import {PORT} from './config.js';
-import express from 'express';
+import { PORT } from "./config.js";
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
-app.get('/', (req,res) => {
-    res.send("home route")
-})
-app.listen(PORT, () => {
-    console.log('Server running on PORT', PORT);
-})
+app.get("/", (req, res) => {
+  res.send("home route");
+});
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server running on PORT", PORT);
+    });
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("Database not connected", error);
+  });
